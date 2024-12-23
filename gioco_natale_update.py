adc54c687983e1cafe946fef13c91d812d2e20c7
Federica Graziano <161356656+FedericaGraziano@users.noreply.github.com>
2024-12-23 20:54:34 +0100

    Update gioco_natale_update.py
    final changes

--- gioco_natale_update.py
+++ gioco_natale_update.py
@@ -93,7 +93,7 @@ def livello3():
     st.title("🎅 Livello 3: Indovina il Regalo di Natale! 🎁")
     st.write("Hai **3 tentativi** per indovinare il regalo misterioso. Buona fortuna!")
     
-    regali = ["bambola", "trenino", "cioccolato", "videogioco", "libro", "puzzle", "orsacchiotto"]
+    regali = ["bambola", "trenino", "cioccolato", "libro", "puzzle", "orsacchiotto"]
     if not st.session_state.regalo_misterioso:
         st.session_state.regalo_misterioso = random.choice(regali)
     regalo_misterioso = st.session_state.regalo_misterioso
@@ -130,10 +130,10 @@ def mostra_risultato():
     if st.session_state.punteggio <= 3:
         st.error("Hai bisogno di ripassare ancora un po' prima di festeggiare il Natale! A meno che tu non sia il Grinch!")
         st.video("https://www.youtube.com/watch?v=nytpYtLtHpE")
-    elif 4 <= st.session_state.punteggio < 5:
+    elif 4 <= st.session_state.punteggio <= 5:
         st.warning("Bravo! Sei pronto per festeggiare il Natale, ma puoi migliorare!")
         st.video("https://www.youtube.com/watch?v=xv-y0xhMqkI")
-    elif 5 <= st.session_state.punteggio <= 7:
+    elif 6 <= st.session_state.punteggio <= 7:
         st.success("Sei il re del Natale!")
         st.video("https://www.youtube.com/watch?v=8nMsY0_7FYA")
 
